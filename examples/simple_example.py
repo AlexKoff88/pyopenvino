95d4c2e07d8786c47f999eee17b3922f49d297cc
Alexander <kozzzloff@list.ru>
2022-11-11 10:01:35 +0400

    Added basic options

--- examples/simple_example.py
+++ examples/simple_example.py
@@ -3,6 +3,7 @@ import numpy as np
 import torch
 import torchvision.models as models
 
+import openvino.runtime as ov
 import pyopenvino as pyov
 
 MODEL_LOCAL_PATH="mobilenet_v2.onnx"
@@ -16,11 +17,28 @@ def get_onnx_model(model):
 torch_model = models.mobilenet_v2(pretrained=True) 
 torch_model.eval()
 
+input = np.random.randint(255, size=(1,3,224,224), dtype=np.uint8).astype(float)
 get_onnx_model(torch_model)
 
+## Create from file
 model = pyov.Model.from_file(MODEL_LOCAL_PATH)
-
-input = np.random.randint(255, size=(1,3,224,224), dtype=np.uint8).astype(float)
 result = model(input)
+print(f"From file: {np.argmax(result['output'])}")
 
-print(f"Class index: {np.argmax(result['output'])}")
+## Create from openvino.runtime.Model
+core = ov.Core()
+ov_model = core.read_model(MODEL_LOCAL_PATH)
+model = pyov.Model(ov_model)
+result = model(input)
+print(f"From model: {np.argmax(result['output'])}")
+
+## Select inference device
+# model.to("GPU")
+
+## Lower weights precision to FP16. GPU switches to FP16 inference
+model.half()
+result = model(input)
+print(f"FP16 weights: {np.argmax(result['output'])}")
+
+## Save to file
+model.save("tmp.xml")
--- pyopenvino/model.py
+++ pyopenvino/model.py
@@ -1,10 +1,13 @@
 import openvino.runtime as ov
+from openvino.offline_transformations import compress_model_transformation
 
 class Model():
     def __init__(self, model: ov.Model=None):
         self._model = model
         self._compiled_model = None
         self._core = ov.Core()
+        self._device = "CPU"
+        self._config = None
 
     @classmethod
     def from_file(cls, file_name: str):
@@ -12,14 +15,46 @@ class Model():
         model = core.read_model(model=file_name)
         return cls(model)
 
-    def __call__(self, inputs) -> dict:
+    def __call__(self, inputs: dict) -> dict:
         if not self._compiled_model:
-            self._compiled_model = self._core.compile_model(self._model)
-            self.outputs_map = {next(iter(output.names)): output for output in self._compiled_model.outputs}
+            self._compiled_model = self._core.compile_model(self._model, self._device, self._config)
 
         infer_result = self._compiled_model(inputs)
         result = {next(iter(output.names)): value for (output, value) in infer_result.items()}
         return result
+    
+    def to(self, device: str, config: dict=None):
+        """
+        Select inference device and settings
+
+        Arguments:
+            device (`str`):
+                Device name, e.g. "CPU" or "GPU"
+            config (dict):
+                OpenVINO config with inference options
+        """
+        self._device = device
+        self._config = config
+        self._compiled_model = None
+        return self
+
+    def half(self):
+        """
+        Lower weights precision to FP16. GPU switches to FP16 inference
+        """
+        compress_model_transformation(self._model)
+        self._compiled_model = None
+        return self
+
+    def save(self, file_name: str):
+        """
+        Save model to file
+
+        Arguments:
+            file_name (`str`):
+                File to store the model. Should contain .xml extension.
+        """
+        ov.serialize(self._model, file_name)
 
     def __getattr__(self, attr):
         if attr in self.__dict__:
