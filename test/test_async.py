ac1cffcfd7e784fa1b31a96f84864c5800f4e54a
AlexKoff88 <alexander.kozlov@intel.com>
2022-11-12 10:18:11 +0400

    Removed torch dependency in tests

--- test/test_async.py
+++ test/test_async.py
@@ -1,6 +1,8 @@
 import numpy as np
 import pytest
 import tempfile
+import requests
+import os
 
 import torch
 import torchvision.models as models
@@ -8,12 +10,13 @@ import torchvision.models as models
 import openvino.runtime as ov
 import pyopenvino as pyov
 
+MODEL_PATH = tempfile.NamedTemporaryFile(suffix=".onnx").name
 
-def get_onnx_model(model, file_name):
-    dummy_input = torch.randn(1, 3, 224, 224)
-    input_names = ["input"] 
-    output_names = ["output"]
-    torch.onnx.export(model, dummy_input, file_name, verbose=False, input_names=input_names, output_names=output_names)
+def download_model(file_name):
+    URL = "https://github.com/onnx/models/raw/main/vision/classification/mobilenet/model/mobilenetv2-12.onnx"
+    if not os.path.exists(file_name):
+        r = requests.get(URL, allow_redirects=True)
+        open(file_name, 'wb').write(r.content)
 
 @pytest.mark.parametrize(("total_infers", "workers"), 
             [(10,2), (20,4)])
@@ -24,12 +27,9 @@ def test_async(total_infers, workers):
         print(f"Done! Number: {userdata}")
         results[userdata] = True
 
-    torch_model = models.mobilenet_v2(pretrained=True) 
-    torch_model.eval()
-    with tempfile.NamedTemporaryFile(suffix=".onnx") as tmp:
-        get_onnx_model(torch_model, tmp.name)
-        ## Create Model from file
-        model = pyov.Model.from_file(tmp.name)
+    download_model(MODEL_PATH)
+    ## Create Model from file
+    model = pyov.Model.from_file(MODEL_PATH)
     
     model.workers = workers
     model.callback = callback
@@ -40,3 +40,8 @@ def test_async(total_infers, workers):
 
     model.wait()
     assert all(results)
+
+@pytest.fixture(scope='session', autouse=True)
+def clean_up():
+    if os.path.exists(MODEL_PATH):
+        os.remove(MODEL_PATH)
