37497738882e3ce85bb728427cdc1aad72ed8647
Alexander <kozzzloff@list.ru>
2022-11-22 16:14:04 +0400

    Added CELoss (doesn't converge with torch yet)

--- pyopenvino/training/losses.py
+++ pyopenvino/training/losses.py
@@ -1,6 +1,6 @@
 from abc import ABC, abstractmethod
 from typing import List
-import copy
+import math
 
 import openvino.runtime as ov
 from openvino.runtime import opset9
@@ -20,16 +20,16 @@ class L2Loss(BaseLoss):
         # Create subgraph for L2
         input = opset9.parameter(target_shape, name="input")
         target = opset9.parameter(target_shape, name="target")
-        l2 = opset9.squared_difference(input, target, name="l2")
-        shapeof = opset9.shape_of(l2, name="shape")
-        shape_of_shape = opset9.shape_of(shapeof, name="shape_of_shape")
+        l2 = opset9.squared_difference(input, target, name="l2_loss")
+        shapeof = opset9.shape_of(l2, name="shape_loss")
+        shape_of_shape = opset9.shape_of(shapeof, name="shape_of_shape_loss")
         axis = opset9.constant(0)
-        axis_end = opset9.squeeze(shape_of_shape, axis, name="squeeze")
+        axis_end = opset9.squeeze(shape_of_shape, axis, name="squeeze_loss")
         axis_start = opset9.constant(1)
         step = opset9.constant(1)
-        shape = opset9.range(axis_start, axis_end, step, name="shape")
+        shape = opset9.range(axis_start, axis_end, step, name="shape_loss")
 
-        mean = opset9.reduce_mean(l2, shape, name="mean")
+        mean = opset9.reduce_mean(l2, shape, name="mean_loss")
         result = opset9.result(mean, name="l2_loss")
 
         self._model = ov.Model([result], [input, target])
@@ -41,6 +41,58 @@ class L2Loss(BaseLoss):
 
         new_params = list(model.get_parameters()) + [self.get_operation("target")]
         new_results = [self.get_operation("l2_loss")]
+
+        ov_model = ov.Model(new_results, new_params)
+        return Model(ov_model)
+
+    def __getattr__(self, attr):
+        if attr in self.__dict__:
+            return getattr(self, attr)
+        elif attr in dir(self._model):
+            self._compiled_model = None
+            return getattr(self._model, attr)
+        elif self._compiled_model is not None:
+            return getattr(self._compiled_model, attr)
+        raise ValueError(f"Unknown attribute: {attr}")
+
+
+class CELoss(BaseLoss):
+    def __init__(self, target_shape):
+        super(CELoss, self).__init__()
+
+        # Create subgraph for CELoss
+        input = opset9.parameter(target_shape, name="input")
+        target = opset9.parameter(target_shape, name="target")
+        softmax = opset9.softmax(input, axis=1, name="softmax_loss")
+        log = opset9.log(softmax, name="log_loss")
+        mul = opset9.multiply(log, target)
+
+        shapeof = opset9.shape_of(mul, name="shape_loss")
+        shape_of_shape = opset9.shape_of(shapeof, name="shape_of_shape_loss")
+        axis = opset9.constant(0)
+        axis_end = opset9.squeeze(shape_of_shape, axis, name="squeeze_loss")
+        axis_start = opset9.constant(0)
+        step = opset9.constant(1)
+        shape = opset9.range(axis_start, axis_end, step, name="shape_loss")
+
+        sum = opset9.reduce_sum(mul, shape, name="sum_loss")
+        minus_one = opset9.constant(-1.0 / math.log(2), dtype=float)
+        negate = opset9.multiply(sum, minus_one, name="negate_loss")
+        batch_axis = opset9.constant(0)
+        batch_size = opset9.gather(shapeof, batch_axis, batch_axis)
+        convert = opset9.convert(batch_size, destination_type=float, name="convert_loss")
+        divide = opset9.divide(negate, convert, name="divide_loss")
+
+        result = opset9.result(divide, name="ce_loss")
+        self._model = ov.Model([result], [input, target])
+
+    def attach(self, model: Model, result_name: str) -> Model:
+        param_node = self.get_input("input")
+        result_node = model.get_output(result_name)
+        param_node.output(0).replace(result_node.input(0).get_source_output())
+
+        new_params = list(model.get_parameters()) + [self.get_operation("target")]
+        new_results = [self.get_operation("ce_loss")]
 
         ov_model = ov.Model(new_results, new_params)
         return Model(ov_model)
--- test/test_training.py
+++ test/test_training.py
@@ -6,7 +6,7 @@ import torch
 from torchvision import transforms
 from torchvision import datasets
 
-from pyopenvino.training.losses import L2Loss
+from pyopenvino.training.losses import L2Loss, CELoss
 import openvino.runtime as ov
 
 import numpy as np
@@ -15,6 +15,7 @@ import pyopenvino as pyov
 
 MODEL_PATH = tempfile.NamedTemporaryFile(suffix=".onnx").name
 DATASET_PATH = "./dataset/"
+NUM_CLASSES = 101
 
 def download_model(file_name):
     #URL = "https://github.com/onnx/models/raw/main/vision/classification/mobilenet/model/mobilenetv2-12.onnx"
@@ -23,16 +24,39 @@ def download_model(file_name):
         r = requests.get(URL, allow_redirects=True)
         open(file_name, 'wb').write(r.content)
 
-def test_l2_loss_inference():
-    loss = L2Loss([1,100])
-    '''loss.save("l2loss.xml")
+def test_ce_loss_inference():
+    loss = CELoss([1,NUM_CLASSES])
+    loss.save("celoss.xml")
 
-    print(f"Inputs: {loss.get_parameters()}")'''
+    #print(f"Inputs: {loss.get_parameters()}")
     
-    x = np.random.rand(1,100)
+    x = np.random.rand(1,NUM_CLASSES)
+    target = np.array([1], dtype=int)
+    one_hot = np.full((2, 2), np.inf) #np.zeros((1,NUM_CLASSES))
+    one_hot[0][target] = 1
+    result = loss({"input":x, "target": one_hot})
+    print(f"One hot: {one_hot}")
+
+    ref_loss = torch.nn.CrossEntropyLoss(reduction="sum")
+    ref_x = torch.from_numpy(x)
+    ref_target = torch.from_numpy(target)
+    ref_result = ref_loss(ref_x, ref_target)
+
+    print(f"Target: {target}, Ref target: {ref_target}, Results: {result}, Ref result: {ref_result}")
+
+    #print(f"result: {result}")
+    assert result["output0"] == ref_result.numpy()
+
+def test_l2_loss_inference():
+    loss = L2Loss([1,NUM_CLASSES])
+    loss.save("l2loss.xml")
+
+    #print(f"Inputs: {loss.get_parameters()}")
+    
+    x = np.random.rand(1,NUM_CLASSES)
     result = loss({"input":x, "target": x})
 
-    print(f"result: {result}")
+    #print(f"result: {result}")
     assert result["output0"] == 0
 
 def test_l2_loss_attach():
@@ -40,23 +64,28 @@ def test_l2_loss_attach():
     model = pyov.Model.from_file(MODEL_PATH)
     result_name = model.get_result().get_friendly_name()
     
-    loss = L2Loss([-1,100])
+    loss = L2Loss([1,NUM_CLASSES])
     new_model = loss.attach(model, result_name)
-    assert new_model
+
+    x = np.random.rand(1,3,224,224)
+    label = np.random.rand(1,NUM_CLASSES)
+    model_input = {"input": x, "target": label}
+    result = new_model(model_input)
+    assert result
 
 def test_e2e():
     download_model(MODEL_PATH)
     model = pyov.Model.from_file(MODEL_PATH)
     result_name = model.get_result().get_friendly_name()
     
-    loss = L2Loss([-1,100])
+    loss = L2Loss([1,NUM_CLASSES])
     new_model = loss.attach(model, result_name)
 
     normalize = transforms.Normalize(mean=[0.485, 0.456, 0.406],
                                      std=[0.229, 0.224, 0.225])
     val_dataset = datasets.Food101(
-        root="DATASET_PATH",
-        split = 'test', 
+        root=DATASET_PATH,
+        split = "test", 
         transform = transforms.Compose([
             transforms.Resize(256),
             transforms.CenterCrop(224),
@@ -69,11 +98,15 @@ def test_e2e():
         val_dataset, batch_size=1, shuffle=False,
         num_workers=0)
 
-    for i, (data, target) in enumerate(val_loader):
-        model_input = {"input": data.numpy(), "target": target.numpy()}
+    for i, (data, label) in enumerate(val_loader):
+        #print(f"Data shape: {data.shape}, target shape: {label.shape}")
+        target = np.zeros((data.size(0),NUM_CLASSES))
+        target[0][0] = 1 #target[0][label.item()] = 1
+        print(f"Label: {label.item()}, Target: {target}")
+        model_input = {"input": data.numpy(), "target": target}
         loss_val = new_model(model_input)
-        print(f"Loss: {loss_val}")
-        if i>10:
+        print(f"Loss: {loss_val['output0'][0]}")
+        if i>1:
             break
 
 
